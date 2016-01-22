82cc92fdfac4889d8df42c6fe1321e48ce7c7822
Graphic <graphic@SOD-SJH-516-22M.local>
2016-01-22 12:43:16 -0500

    week 1

---
+++ WEEK 1/example1/scripts/main.js
@@ -0,0 +1,9 @@
+var myHeading = document.querySelector('h1');
+
+	myHeading.textContent = 'Hello World!';
+
+//length of objects, in this case the letters including spaces
+//alert(myHeading.textContent.length);
+
+//0 calles H, 0 is always the first element
+alert(myHeading.textContent[0]);
