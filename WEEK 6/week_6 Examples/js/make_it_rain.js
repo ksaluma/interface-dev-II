bee88fa82a4577038e54601d53512a3ac812d4d3
Graphic <graphic@SOD-SJH-516-22M.local>
2016-02-19 12:15:14 -0500

    start of class

---
+++ WEEK 6/week_6 Examples/js/make_it_rain.js
@@ -0,0 +1,50 @@
+function add(n1,n2){
+    return(n1+n2);
+}
+
+function subtract(n1,n2){
+    return(n1-n2);
+}
+
+function multiply(n1,n2){
+    return(n1*n2);
+}
+
+function divide(n1,n2){
+    return(n1/n2);
+}
+
+function makeItRain(n1, n2, con){
+var result;
+    switch(con){
+    case 'add':
+        
+        
+    case 'subtract':
+        
+        
+    case 'multiply':
+        
+            
+    case 'divide':
+        
+            
+    default: 
+        result = "Welcome to Week 6";
+    
+    }
+    return result;
+}
+
+
+function contater(n){
+    var num
+    //test n by parsing the integer
+    if (){
+    //if it is a number, convert it to a string
+   
+    }else{
+        
+    }
+    
+}
